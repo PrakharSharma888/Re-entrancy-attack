344f914f331daf00f611d337387938e2e5a4fc90
PrakharSharma888 <prakharentp2002@gmail.com>
2022-10-03 14:53:54 +0530

    BuildBear

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,10 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  defaultNetwork: "buildbear",
+  networks: {
+    buildbear: {
+      url: "https://backend.buildbear.io/node/gallant-liskov-53082d"
+    }
+  }
 };
